9e5fa4371dadb70907ef90cb280acc19fedae1ec
KurangKering <cybercature@gmail.com>
2021-07-14 03:14:58 +0700

    deploy

--- sistem/sistem/settings.py
+++ sistem/sistem/settings.py
@@ -15,7 +15,6 @@ import os
 import django_heroku
 import dj_database_url
 from decouple import config
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -78,6 +77,7 @@ WSGI_APPLICATION = 'sistem.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 # These lines make all the magic
 DATABASES = {
     'default': dj_database_url.config(
@@ -118,9 +118,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 STATIC_URL = '/static/'
@@ -128,10 +125,12 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     BASE_DIR / 'static',
 )
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 
 import dj_database_url
